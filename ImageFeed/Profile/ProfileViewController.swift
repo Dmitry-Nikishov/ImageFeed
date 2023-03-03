f00f486da3ae5d7225eb3cc7e89f64ee3ce645d0
Dmitry Nikishov <dmitry86nik@gmail.com>
2023-03-03 13:59:22 +0300

    profile stage storyboard layout

---
+++ ImageFeed/Profile/ProfileViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ProfileViewController.swift
+//  ImageFeed
+//
+//  Created by Дмитрий Никишов on 02.03.2023.
+//
+
+import UIKit
+
+final class ProfileViewController: UIViewController {
+    @IBOutlet private var avatarImageView: UIImageView!
+    @IBOutlet private var logoutButton: UIButton!
+    @IBOutlet private var nameLabel: UILabel!
+    @IBOutlet private var loginNameLabel: UILabel!
+    @IBOutlet private var descriptionLabel: UILabel!
+    
+    @IBAction private func didTapLogoutButton() {
+        
+    }
+}
